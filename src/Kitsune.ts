c970b26d73ea18319e04a7fbb852d23aac11a040
Praise Jacob <iampraisejacob@gmail.com>
2026-09-08 23:25:42 +0530

    feat: auto reload style and config

--- src/Kitsune.ts
+++ src/Kitsune.ts
@@ -3,13 +3,18 @@ import app from "ags/gtk4/app";
 import Bar from "@/Bar";
 import Config from "@/Config";
 import { Frame } from "@/Frame";
+import { monitorConfigChanges, monitorStyleChanges } from "@/utils/monitor";
+
+console.dir(Config, {} as never);
 
 export const KitsuneOptions: Parameters<typeof app.start>[0] = {
 	instanceName: Config.App.name,
 	main: () => {
 		GLib.set_prgname(Config.App.id);
-		const monitors = app.get_monitors();
+		monitorStyleChanges();
+		monitorConfigChanges();
 
+		const monitors = app.get_monitors();
 		const primary =
 			monitors.find((m) => {
 				const geo = m.get_geometry();
---
+++ src/utils/monitor.ts
@@ -0,0 +1,45 @@
+import GLib from "gi://GLib";
+import { monitorFile } from "ags/file";
+import app from "ags/gtk4/app";
+import { exec } from "ags/process";
+import Config, { getConfigDir } from "@/Config";
+
+const files = (() => {
+	const baseDir = getConfigDir();
+	const dir = GLib.Dir.open(baseDir, 0);
+	const files: Array<string> = [];
+
+	let name = dir.read_name();
+	while (name) {
+		const path = GLib.build_filenamev([baseDir, name]);
+		if (GLib.file_test(path, GLib.FileTest.IS_REGULAR)) {
+			files.push(name);
+		}
+		name = dir.read_name();
+	}
+
+	return files;
+})();
+
+export function monitorStyleChanges() {
+	const buildTarget = exec(["mktemp", "--suffix", ".css"]);
+	exec(["sass", Config.Files.styles, buildTarget]);
+
+	files
+		.filter((file) => file.endsWith(".scss"))
+		.forEach((file) => {
+			monitorFile(file, () => {
+				exec(["sass", Config.Files.styles, buildTarget]);
+				app.reset_css();
+				app.apply_css(buildTarget);
+			});
+		});
+}
+
+export function monitorConfigChanges() {
+	const configDir = getConfigDir();
+	const target = GLib.build_filenamev([configDir, "kitsune"]);
+	monitorFile(target, () => {
+		app.quit();
+	});
+}
