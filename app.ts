74f53f5aa6d3376d9b7c0b2643c3444429d3fe22
Praise Jacob <iampraisejacob@gmail.com>
2026-09-08 05:31:05 +0530

    feat: auto detect primary display

--- app.ts
+++ app.ts
@@ -1,10 +1,22 @@
+import type { Gdk } from "ags/gtk4";
 import app from "ags/gtk4/app";
+import Bar from "@/Bar";
+import { Frame } from "@/Frame";
+
 import style from "./style.scss";
-import Bar from "./widget/Bar";
 
 app.start({
 	css: style,
 	main() {
-		app.get_monitors().map(Bar);
+		const monitors = app.get_monitors();
+
+		const primary =
+			monitors.find((m) => {
+				const geo = m.get_geometry();
+				return geo.x === 0 && geo.y === 0;
+			}) ?? (monitors[0] as Gdk.Monitor);
+
+		Bar(primary);
+		Frame(primary);
 	},
 });
