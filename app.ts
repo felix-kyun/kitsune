a1c083bfbcc5c9aff9ede574ec7c7f6187e99db1
Praise Jacob <iampraisejacob@gmail.com>
2026-09-08 05:56:29 +0530

    chore: split main entrypoint

--- app.ts
+++ app.ts
@@ -1,22 +1,8 @@
-import type { Gdk } from "ags/gtk4";
 import app from "ags/gtk4/app";
-import Bar from "@/Bar";
-import { Frame } from "@/Frame";
-
+import { MiriOptions } from "@/Miri";
 import style from "./style.scss";
 
 app.start({
 	css: style,
-	main() {
-		const monitors = app.get_monitors();
-
-		const primary =
-			monitors.find((m) => {
-				const geo = m.get_geometry();
-				return geo.x === 0 && geo.y === 0;
-			}) ?? (monitors[0] as Gdk.Monitor);
-
-		Bar(primary);
-		Frame(primary);
-	},
+	...MiriOptions,
 });
